515522e8931288f25484caee7fe2b0ea257d4369
mooncityorg <cryptomoon116@gmail.com>
2024-09-17 03:23:40 -0700

    update

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,7 +1,7 @@
 export const X = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="full" height="full" viewBox="0 0 16 16"><path fill="#1e9600" d="M9.294 6.928L14.357 1h-1.2L8.762 6.147L5.25 1H1.2l5.31 7.784L1.2 15h1.2l4.642-5.436L10.751 15h4.05zM7.651 8.852l-.538-.775L2.832 1.91h1.843l3.454 4.977l.538.775l4.491 6.47h-1.843z" /></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="#1e9600" d="M9.294 6.928L14.357 1h-1.2L8.762 6.147L5.25 1H1.2l5.31 7.784L1.2 15h1.2l4.642-5.436L10.751 15h4.05zM7.651 8.852l-.538-.775L2.832 1.91h1.843l3.454 4.977l.538.775l4.491 6.47h-1.843z" /></svg>
 )
 
 export const DocumentIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="full" height="full" viewBox="0 0 32 32"><path fill="#1e9600" d="m25.7 9.3l-7-7c-.2-.2-.4-.3-.7-.3H8c-1.1 0-2 .9-2 2v24c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V10c0-.3-.1-.5-.3-.7M18 4.4l5.6 5.6H18zM24 28H8V4h8v6c0 1.1.9 2 2 2h6z" /><path fill="#1e9600" d="M10 22h12v2H10zm0-6h12v2H10z" /></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="#1e9600" d="m25.7 9.3l-7-7c-.2-.2-.4-.3-.7-.3H8c-1.1 0-2 .9-2 2v24c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V10c0-.3-.1-.5-.3-.7M18 4.4l5.6 5.6H18zM24 28H8V4h8v6c0 1.1.9 2 2 2h6z" /><path fill="#1e9600" d="M10 22h12v2H10zm0-6h12v2H10z" /></svg>
 )
