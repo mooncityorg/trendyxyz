26ff280e07cbcdbec1d34a4a92020ebf56f379d2
mooncityorg <cryptomoon116@gmail.com>
2024-09-17 03:12:41 -0700

    update

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,4 +4,4 @@ export const X = () => (
 
 export const DocumentIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="full" height="full" viewBox="0 0 32 32"><path fill="#1e9600" d="m25.7 9.3l-7-7c-.2-.2-.4-.3-.7-.3H8c-1.1 0-2 .9-2 2v24c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V10c0-.3-.1-.5-.3-.7M18 4.4l5.6 5.6H18zM24 28H8V4h8v6c0 1.1.9 2 2 2h6z" /><path fill="#1e9600" d="M10 22h12v2H10zm0-6h12v2H10z" /></svg>
-)
+)           
